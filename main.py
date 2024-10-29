4875f2ef6eb40d8210073807e60c19fa80588241
eloa <eloalav@gmail.com>
2024-10-29 13:59:17 -0300

    Site_confeitaria

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = '/uploads'
 
 @app.route("/")
 def inicial():
@@ -22,7 +24,28 @@ def home():
 def contato():
     return render_template("contato.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
+    if username == "eloalav":
+        if password == "eloa123":
+            return redirect(url_for('upload'))
+
+@app.route("/upload")
+def upload():
+    if request.method == "GET":
+     return render_template("upload.html")
+    elif request.method == "POST":
+        file = request.files["file"]
+        caminho = app.config["UPLOAD_FOLDER"] + "/" + file.filename
+        file.save(caminho)
+
+        return render_template("cardapio.html")
 
 
 if __name__ == '__main__':
